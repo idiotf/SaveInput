1b0aeb5a52d3ea30fb01de89e3a04a7669cee015
idiotf <140891554+idiotf@users.noreply.github.com>
2023-07-29 20:33:16 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+setInterval(function() {
+	"use strict";
+	const submitButton = document.querySelectorAll(".e13821ld2");
+	submitButton.forEach(button => {
+		if(button.className.includes("event")) return;
+		button.addEventListener("click", function() {
+			const write = button.parentNode.parentNode.parentNode.querySelector("#Write");
+			localStorage.setItem("saveInput", write.value);
+			const interval = setInterval(function() {
+				if(!write.value) {
+					write.value = localStorage.getItem("saveInput");
+					clearInterval(interval);
+				}
+			});
+		});
+		button.classList.add("event");
+	});
+});
