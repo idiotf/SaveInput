9be06aa7a2081483fb613af53a599de24b651b77
idiotf <140891554+idiotf@users.noreply.github.com>
2023-09-02 11:37:59 +0900

    Update main.js
    textarea의 value의 setter를 편집해서 설정을 못하게 함.
    __defineSetter__가 문제 되면 알림 부탁.

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 setInterval(function() {
 	"use strict";
-	const submitButton = document.querySelectorAll(".e13821ld2");
-	submitButton.forEach(button => {
-		if(button.className.includes("event")) return;
-		button.addEventListener("click", function() {
-			const write = button.parentNode.parentNode.parentNode.querySelector("#Write");
-			localStorage.setItem("saveInput", write.value);
-			const interval = setInterval(function() {
-				if(!write.value) {
-					write.value = localStorage.getItem("saveInput");
-					write.addEventListener("keydown", function() {
-						clearInterval(interval);
-					});
-				}
-			});
-		});
-		button.classList.add("event");
-	});
+	//const submitButton = document.querySelectorAll(".e13821ld2");
+	//submitButton.forEach(button => {
+	//	if(button.className.includes("event")) return;
+	//	button.addEventListener("click", function() {
+	//		const write = button.parentNode.parentNode.parentNode.querySelector("#Write");
+	//		localStorage.setItem("saveInput", write.value);
+	//		const interval = setInterval(function() {
+	//			if(!write.value) {
+	//				write.value = localStorage.getItem("saveInput");
+	//				write.addEventListener("keydown", function() {
+	//					clearInterval(interval);
+	//				});
+	//			}
+	//		});
+	//	});
+	//	button.classList.add("event");
+	//});
+	const allTextarea = document.getElementsByName("Write");
+	for(const textarea of allTextarea) {
+		textarea.__defineSetter__("value", function() {}); // 이 코드가 문제 되면 알림 부탁
+	}
 });
