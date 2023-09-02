f6c7c0dafa74e74b8fa509ebabd01f59e94e521e
idiotf <140891554+idiotf@users.noreply.github.com>
2023-09-02 11:45:08 +0900

    1.1 업데이트

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ setInterval(function() {
 	//	});
 	//	button.classList.add("event");
 	//});
-	const allTextarea = document.getElementsByName("Write");
+	const allTextarea = document.querySelectorAll("#Write");
 	for(const textarea of allTextarea) {
 		textarea.__defineSetter__("value", function() {}); // 이 코드가 문제 되면 알림 부탁
 	}
