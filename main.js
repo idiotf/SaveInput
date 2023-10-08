689cd5b3a4ccb2c938e85a8e62c0f9490d2fb637
idiotf <140891554+idiotf@users.noreply.github.com>
2023-10-08 10:53:08 +0900

    (업데이트 중)

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 setInterval(function() {
 	"use strict";
-	//const submitButton = document.querySelectorAll(".e13821ld2");
-	//submitButton.forEach(button => {
-	//	if(button.className.includes("event")) return;
-	//	button.addEventListener("click", function() {
-	//		const write = button.parentNode.parentNode.parentNode.querySelector("#Write");
-	//		localStorage.setItem("saveInput", write.value);
-	//		const interval = setInterval(function() {
-	//			if(!write.value) {
-	//				write.value = localStorage.getItem("saveInput");
-	//				write.addEventListener("keydown", function() {
-	//					clearInterval(interval);
-	//				});
-	//			}
-	//		});
-	//	});
-	//	button.classList.add("event");
-	//});
-	document.querySelectorAll("#Write").forEach(function(textarea) {
-		if(textarea.value) textarea.__defineSetter__("value", function() {}); // 이 코드가 문제 되면 알림 부탁
+	const submitButton = document.querySelectorAll(".e13821ld2");
+	submitButton.forEach(button => {
+		if(button.className.includes("event")) return;
+		button.addEventListener("click", function() {
+			const write = button.parentNode.parentNode.parentNode.querySelector("#Write");
+			localStorage.setItem("saveInput", write.value);
+			const interval = setInterval(function() {
+				if(!write.value) {
+					write.value = localStorage.getItem("saveInput");
+					write.addEventListener("input", function() {
+						clearInterval(interval);
+					});
+				}
+			});
+		});
+		button.classList.add("event");
 	});
 });
